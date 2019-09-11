59c60dbfdecf6f59d79aae64559039f4d602f8b1
Ethan Davies <ethanmdavies2002@gmail.com>
2019-09-10 21:14:35 -0700

    Fixed up the shape program main to work, probably

--- ChooseShape.java
+++ ChooseShape.java
@@ -29,62 +29,6 @@ public class ChooseShape
        angle = 180 - ((sides - 2) * 180)/sides;
        apothem = (100)/(2*(Math.tan(3.14/sides)));
        initial_forward =(int)apothem;
-       pencil.up();
-       pencil.home();
-       pencil.down();
-       pencil.drawCircle(50);
-       pencil.up();
-       pencil.forward(initial_forward);
-       pencil.down();
-       pencil.turnRight(90);
-       pencil.forward(50);
-       for(x = 0; x<sides; x++){
-           pencil.turnRight(angle);
-           pencil.forward(100); 
-       }
-       /*
-       pencil.up();
-       pencil.home();
-       pencil.turnLeft(90);
-       pencil.forward(20);
-       pencil.turnRight(90);
-       pencil.down();
-       pencil.drawString("Test 1");
-
-       for(x = 0; x < 100; x++)
-       {
-           pencil.turnLeft(1);
-           pencil.forward(50);
-           pencil.drawString("Test 1");
-           pencil.turnRight(100);
-           pencil.forward(50);
-        }
-        */
-       /*
-       pencil.up();
-       pencil.home();
-       pencil.turnLeft(180);
-       pencil.forward(75);
-       pencil.turnRight(90);
-       pencil.down();
-       int y;
-       for(x=0; x<100; x++)
-       {
-           if (x % 10 == 0)
-           {
-               pencil.drawCircle(25);
-           }
-           if (x % 15 == 0)
-           {
-               pencil.drawCircle(50);
-           }
-           pencil.drawCircle(15);
-           pencil.up();
-           pencil.forward(5);
-           pencil.down();
-           pencil.turnRight(3.6); 
-           }  
-           */
     }
     
     public static void getColor(DrawingTool pencil, ConsoleIO keyboard)
--- DrawObject.java
+++ DrawObject.java
@@ -1,4 +1,7 @@
-
+import java.util.*;
+import chn.util.*;
+import apcslib.*;
+import java.awt.Color;
 public class DrawObject
 {
     // instance variables - replace the example below with your own
@@ -7,6 +10,8 @@ public class DrawObject
     int angle_size;
     double initial_forward;
     double height;
+    DrawingTool pencil;
+    SketchPad paper;
     
     /**
      * Constructor for objects of class DrawObject
@@ -18,11 +23,67 @@ public class DrawObject
         angle_size = angle;
         height = apothem;
         initial_forward = initial_forward;
+        paper = new SketchPad(500, 500); 
+        pencil = new DrawingTool(paper);
     }
 
-    public int sampleMethod()
+    public int Art()
     {
-        // put your code here
-        return x + y;
+       pencil.up();
+       pencil.home();
+       pencil.down();
+       pencil.drawCircle(50);
+       pencil.up();
+       pencil.forward(initial_forward);
+       pencil.down();
+       pencil.turnRight(90);
+       pencil.forward(50);
+       for(x = 0; x<num_sides; x++){
+           pencil.turnRight(angle_size);
+           pencil.forward(100); 
+       }
+       /*
+       pencil.up();
+       pencil.home();
+       pencil.turnLeft(90);
+       pencil.forward(20);
+       pencil.turnRight(90);
+       pencil.down();
+       pencil.drawString("Test 1");
+
+       for(x = 0; x < 100; x++)
+       {
+           pencil.turnLeft(1);
+           pencil.forward(50);
+           pencil.drawString("Test 1");
+           pencil.turnRight(100);
+           pencil.forward(50);
+        }
+        */
+       /*
+       pencil.up();
+       pencil.home();
+       pencil.turnLeft(180);
+       pencil.forward(75);
+       pencil.turnRight(90);
+       pencil.down();
+       int y;
+       for(x=0; x<100; x++)
+       {
+           if (x % 10 == 0)
+           {
+               pencil.drawCircle(25);
+           }
+           if (x % 15 == 0)
+           {
+               pencil.drawCircle(50);
+           }
+           pencil.drawCircle(15);
+           pencil.up();
+           pencil.forward(5);
+           pencil.down();
+           pencil.turnRight(3.6); 
+           }  
+           */
     }
 }
